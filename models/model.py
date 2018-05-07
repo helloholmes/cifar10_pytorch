05ca6846459b66c3f0635e542d906abd9de6ceda
helloholmes <qqhmql@126.com>
2018-05-07 23:58:15 +0800

    add VGG19

--- models/model.py
+++ models/model.py
@@ -46,3 +46,59 @@ class LeNet(BasicModule):
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
+
+class VGG19(BasicModule):
+    def __init__(self):
+        # the input size: (batch, 3, 32, 32)
+        super(VGG19, self).__init__()
+        self.features = nn.Sequential(nn.Conv2d(3, 64, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(64, 64, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.MaxPool2d(2, 2),
+                                    nn.Conv2d(64, 128, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(128, 128, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.MaxPool2d(2, 2),
+                                    nn.Conv2d(128, 256, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(256, 256, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(256, 256, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(256, 256, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.MaxPool2d(2, 2),
+                                    nn.Conv2d(256, 512, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(512, 512, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(512, 512, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(512, 512, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.MaxPool2d(2, 2),
+                                    nn.Conv2d(512, 512, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(512, 512, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(512, 512, 3, 1, 1),
+                                    nn.ReLU(True),
+                                    nn.Conv2d(512, 512, 3, 1, 1),
+                                    nn.ReLU(True)
+                                    )
+
+        self.classifier = nn.Sequential(nn.Linear(2048, 4096),
+                                        nn.ReLU(True),
+                                        nn.Dropout(p=0.5),
+                                        nn.Linear(4096, 4096),
+                                        nn.ReLU(True),
+                                        nn.Dropout(p=0.5),
+                                        nn.Linear(4096, 10))
+
+    def forward(self, x):
+        x = self.features(x)        
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
