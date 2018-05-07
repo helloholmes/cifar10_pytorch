4b83b9119b07b4dad1777b397af6741c99462fce
helloholmes <qqhmql@126.com>
2018-05-07 23:34:55 +0800

    fist add

---
+++ config.py
@@ -0,0 +1,49 @@
+# coding:utf-8
+'''
+python 3.5
+pytorch 0.3.0
+gym 0.9.4
+author: helloholmes
+'''
+import warnings
+
+class DefaultConfig(object):
+    env = 'default'
+    model = 'LeNet'
+
+    root = './data'
+
+    load_model_path = ''
+    save_model_path = './checkpoints/'
+
+    batch_size = 128
+    use_gpu = True
+    num_workers = 4
+    print_freq = 20
+
+    result_file = 'submission.csv'
+
+    max_epoch = 10
+    lr = 0.001
+    lr_decay = 0.5
+    weight_decay = 0e-5
+
+def parse(self, kwargs):
+    for k, v in kwargs.items():
+        if not hasattr(self, k):
+            warnings.warn('warning: opt has not attribut %s'%k)
+        setattr(self, k, v)
+
+    print('user config:')
+    for k, v in self.__class__.__dict__.items():
+        if not k.startswith('__'):
+            print(k, getattr(self, k))
+
+DefaultConfig.parse = parse
+
+if __name__ == '__main__':
+    opt = DefaultConfig()
+    print(opt.env)
+    new_config = {'env': 'new'}
+    opt.parse(new_config)
+    print(opt.env)
