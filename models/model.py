742018703c5b01cf0eff6159b31115c571cc9bea
helloholmes <qqhmql@126.com>
2018-05-09 22:19:05 +0800

    modify

--- models/model.py
+++ models/model.py
@@ -11,7 +11,7 @@ import time
 import torch.nn.functional as F
 import torchvision.models as models
 from torch import nn
-from resnet import ResNet
+
 
 class BasicModule(nn.Module):
     def __init__(self):
@@ -198,6 +198,73 @@ class NIN(BasicModule):
     def forward(self, x):
         x = self.features(x)
         return x.view(x.size(0), -1)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, stride=1, shortcut=None):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(nn.Conv2d(inchannel, outchannel, 3, stride, 1, bias=False),
+                                nn.BatchNorm2d(outchannel),
+                                nn.ReLU(True),
+                                nn.Conv2d(outchannel, outchannel, 3, 1, 1, bias=False),
+                                nn.BatchNorm2d(outchannel))
+        self.right = shortcut
+
+    def forward(self, x):
+        out = self.left(x)
+        residual = x if self.right is None else self.right(x)
+        out += residual
+        return F.relu(out)
+
+class ResNet(BasicModule):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.pre = nn.Sequential(nn.Conv2d(3, 64, 7, 2, 3, bias=False),
+                                nn.BatchNorm2d(64),
+                                nn.ReLU(True),
+                                nn.MaxPool2d(3, 2, 1))
+
+        self.layer1 = self._make_layer(64, 128, 3)
+        self.layer2 = self._make_layer(128, 256, 4, stride=1)
+        self.layer3 = self._make_layer(256, 512, 6, stride=1)
+        self.layer4 = self._make_layer(512, 512, 3, stride=1)
+
+        self.fc = nn.Linear(512, 10)
+
+        # initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight.data)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, inchannel, outchannel, block_num, stride=1):
+        shortcut = nn.Sequential(nn.Conv2d(inchannel, outchannel, 1, stride, bias=False),
+                                nn.BatchNorm2d(outchannel))
+        layers = []
+        layers.append(ResidualBlock(inchannel, outchannel, stride, shortcut))
+
+        for i in range(1, block_num):
+            layers.append(ResidualBlock(outchannel, outchannel))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.pre(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = F.avg_pool2d(x, 7)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
 
 if __name__ == '__main__':
     data = torch.randn((128, 3, 32, 32))
